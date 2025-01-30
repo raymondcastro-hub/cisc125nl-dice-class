9db525e358a002ca5340a4ee76ab23b57476f3c8
raymondcastro-hub <raymond.castro@student.northampton.edu>
2025-01-30 11:04:27 -0500

    Update

--- dice.h
+++ dice.h
@@ -1,3 +1,16 @@
 // Dice class - declaration
 
 #pragma once
+
+class Dice
+{
+    public:
+        int get_die0();
+        int get_die1();
+        int get_total(); // Get the total value of both dice
+        void roll(); // Roll the dice
+
+    private:
+    int die0; // Value on first die
+    int die1; // Value on second die
+};
